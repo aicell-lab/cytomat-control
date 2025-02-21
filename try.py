f3dd76eb9e3eb6970cd9bbb8f4c7793d4ee9f913
Songtao Cheng <36098954+cccoolll@users.noreply.github.com>
2025-02-21 10:55:19 +0100

    Add configuration file and enhance plate handling logic in try.py

--- try.py
+++ try.py
@@ -3,4 +3,11 @@ from cytomat import Cytomat
 c = Cytomat("COM4")
 
 print("Plate on transfer station?", c.overview_status.transfer_station_occupied)
-c.plate_handler.move_plate_from_transfer_station_to_slot(5)
+# range from 42 to 22, step -1
+for i in range(21, 19, -1):
+
+    c.wait_until_not_busy(timeout=50)
+    c.plate_handler.move_plate_from_transfer_station_to_slot(i)
+    c.wait_until_not_busy(timeout=50)
+    c.plate_handler.move_plate_from_slot_to_transfer_station(i)
+    c.wait_until_not_busy(timeout=50)
